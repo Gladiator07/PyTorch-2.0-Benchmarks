93d1fc21f78a51d886ae0a80bdf0183d06c768fa
Gladiator07 <atharvaaingle@gmail.com>
2023-01-20 00:37:08 +0530

    fp16 + increase bs

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 python main.py \
         --config configs/1_0.yaml \
-        model_name_or_path="bert-base-uncased"
+        model_name_or_path="bert-base-uncased" \
+        per_device_train_batch_size=32 \
+        per_device_eval_batch_size=32
