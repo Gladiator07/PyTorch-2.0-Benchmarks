ae511111462dd39f10ec82a9a6c87c6ec81f2c48
Gladiator07 <atharvaaingle@gmail.com>
2023-01-20 19:39:24 +0530

    fix

--- run_1_0_large.sh
+++ run_1_0_large.sh
@@ -2,23 +2,23 @@
 python src/train.py \
         --config configs/1_0_large.yaml \
         model_name_or_path="bert-large-uncased" \
-        per_device_train_batch_size=24 \
-        per_device_eval_batch_size=24 \
-        learning_rate=3e-5 \
+        training_args.per_device_train_batch_size=24 \
+        training_args.per_device_eval_batch_size=24 \
+        training_args.learning_rate=3e-5 \
         save_artifacts=false
 
 python src/train.py \
         --config configs/1_0_large.yaml \
         model_name_or_path="roberta-large" \
-        per_device_train_batch_size=24 \
-        per_device_eval_batch_size=24 \
-        learning_rate=3e-5 \
+        training_args.per_device_train_batch_size=24 \
+        training_args.per_device_eval_batch_size=24 \
+        training_args.learning_rate=3e-5 \
         save_artifacts=false
         
 python src/train.py \
         --config configs/1_0_large.yaml \
         model_name_or_path="microsoft/deberta-v2-xlarge" \
-        per_device_train_batch_size=16 \
-        per_device_eval_batch_size=16 \
-        learning_rate=2e-5 \
+        training_args.per_device_train_batch_size=16 \
+        training_args.per_device_eval_batch_size=16 \
+        training_args.learning_rate=2e-5 \
         save_artifacts=false
