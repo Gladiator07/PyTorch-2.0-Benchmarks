e770bfff6a2ebd1a6130cd61e41d154c8dbedfab
Gladiator07 <atharvaaingle@gmail.com>
2023-01-20 20:06:36 +0530

    run script

--- run_1_0_large.sh
+++ run_1_0_large.sh
@@ -17,8 +17,24 @@ python src/train.py \
         
 python src/train.py \
         --config configs/1_0_large.yaml \
-        model_name_or_path="microsoft/deberta-v2-xlarge" \
+        model_name_or_path="microsoft/deberta-v3-large" \
         training_args.per_device_train_batch_size=16 \
         training_args.per_device_eval_batch_size=16 \
         training_args.learning_rate=2e-5 \
+        save_artifacts=false
+
+python src/train.py \
+        --config configs/1_0_large.yaml \
+        model_name_or_path="google/electra-large-discriminator" \
+        training_args.per_device_train_batch_size=24 \
+        training_args.per_device_eval_batch_size=24 \
+        training_args.learning_rate=3e-5 \
+        save_artifacts=false
+
+python src/train.py \
+        --config configs/1_0_large.yaml \
+        model_name_or_path="funnel-transformer/xlarge" \
+        training_args.per_device_train_batch_size=24 \
+        training_args.per_device_eval_batch_size=24 \
+        training_args.learning_rate=3e-5 \
         save_artifacts=false
