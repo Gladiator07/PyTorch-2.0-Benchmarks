2f8f4010afa276b72679dd2ba2a8de127c1c553e
Gladiator07 <atharvaaingle@gmail.com>
2023-01-20 12:08:17 +0530

    update run script

--- run_1_0_large.sh
+++ run_1_0_large.sh
@@ -1,12 +1,24 @@
-#!/bin/bash
+# !/bin/bash
 python src/train.py \
         --config configs/1_0_large.yaml \
-        model_name_or_path="bert-large-uncased"
+        model_name_or_path="bert-large-uncased" \
+        per_device_train_batch_size=24 \
+        per_device_eval_batch_size=24 \
+        learning_rate=3e-5 \
+        save_artifacts=false
 
 python src/train.py \
         --config configs/1_0_large.yaml \
-        model_name_or_path="roberta-large"
-
+        model_name_or_path="roberta-large" \
+        per_device_train_batch_size=24 \
+        per_device_eval_batch_size=24 \
+        learning_rate=3e-5 \
+        save_artifacts=false
+        
 python src/train.py \
         --config configs/1_0_large.yaml \
-        model_name_or_path="albert-large-v2"
+        model_name_or_path="albert-large-v2" \
+        per_device_train_batch_size=8 \
+        per_device_eval_batch_size=8 \
+        learning_rate=1e-5 \
+        save_artifacts=false
