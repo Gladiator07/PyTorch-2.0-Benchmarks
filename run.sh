d76c7765ab6573632f429a3e1e3b03605817443e
Gladiator07 <atharvaaingle@gmail.com>
2023-01-20 11:40:50 +0530

    fix file name

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python main.py \
+python train.py \
         --config configs/1_0_base.yaml \
         model_name_or_path="bert-base-uncased"
 
--- run_1_0_large.sh
+++ run_1_0_large.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-python main.py \
+python train.py \
         --config configs/1_0_large.yaml \
         model_name_or_path="bert-large-uncased"
 
-python main.py \
+python train.py \
         --config configs/1_0_large.yaml \
         model_name_or_path="roberta-large"
 
-python main.py \
+python train.py \
         --config configs/1_0_large.yaml \
         model_name_or_path="albert-large-v2"
