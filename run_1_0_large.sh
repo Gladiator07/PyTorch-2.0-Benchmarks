0813aabdd2746102d9f9587b0e0ecc66025785cc
Gladiator07 <atharvaaingle@gmail.com>
2023-01-20 11:41:52 +0530

    fix

--- run_1_0_large.sh
+++ run_1_0_large.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-python train.py \
+python src/train.py \
         --config configs/1_0_large.yaml \
         model_name_or_path="bert-large-uncased"
 
-python train.py \
+python src/train.py \
         --config configs/1_0_large.yaml \
         model_name_or_path="roberta-large"
 
-python train.py \
+python src/train.py \
         --config configs/1_0_large.yaml \
         model_name_or_path="albert-large-v2"
