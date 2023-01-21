47eefea0c76ec27c1d598ddd1128032aff82d412
Gladiator07 <atharvaaingle@gmail.com>
2023-01-21 19:29:01 +0530

    upgrade cuda version to 11.8

--- install_nightly.sh
+++ install_nightly.sh
@@ -1,3 +1,3 @@
-pip install numpy --pre torch[dynamo] --force-reinstall --extra-index-url https://download.pytorch.org/whl/nightly/cu117
+pip3 install numpy --pre torch --force-reinstall --index-url https://download.pytorch.org/whl/nightly/cu118
 pip install git+https://github.com/huggingface/transformers.git@main --upgrade
 pip install datasets evaluate wandb omegaconf sentencepiece
