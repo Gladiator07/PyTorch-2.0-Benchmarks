172fdfc4aea817e9fd93d9828d93465758d4adc5
Gladiator07 <atharvaaingle@gmail.com>
2023-01-22 18:40:30 +0530

    add accelerate dep

--- install_nightly.sh
+++ install_nightly.sh
@@ -1,3 +1,4 @@
 pip3 install numpy --pre torch --force-reinstall --index-url https://download.pytorch.org/whl/nightly/cu118
 pip install git+https://github.com/huggingface/transformers.git@main --upgrade
 pip install datasets evaluate wandb omegaconf sentencepiece
+pip install git+https://github.com/huggingface/accelerate@main
