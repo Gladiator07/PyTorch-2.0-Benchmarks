bc25c6288a3b0394fd8ff7efc33433a49d991fa4
Gladiator07 <atharvaaingle@gmail.com>
2023-01-22 17:29:21 +0530

    fix script name

--- accelerate_1_0.sh
+++ accelerate_1_0.sh
@@ -1,5 +1,5 @@
 # 1.13 + fp32
-accelerate launch src/train.py \
+accelerate launch src/train_accelerate.py \
     --model_name_or_path "bert-large-uncased" \
     --batch_size 16 \
     --learning_rate 2e-5 \
@@ -10,7 +10,7 @@ accelerate launch src/train.py \
     --run_name "1.13_fp32"
 
 # 1.13 + fp16
-accelerate launch src/train.py \
+accelerate launch src/train_accelerate.py \
     --model_name_or_path "bert-large-uncased" \
     --batch_size 24 \
     --learning_rate 3e-5 \
@@ -21,7 +21,7 @@ accelerate launch src/train.py \
     --run_name "1.13_fp16"
 
 # 1.13 + fp16 + gradient_checkpointing
-accelerate launch src/train.py \
+accelerate launch src/train_accelerate.py \
     --model_name_or_path "bert-large-uncased" \
     --batch_size 48 \
     --learning_rate 3e-5 \
@@ -33,7 +33,7 @@ accelerate launch src/train.py \
     --run_name "1.13_gradckpt"
 
 # 1.13 + fp16 + dynamic_padding
-accelerate launch src/train.py \
+accelerate launch src/train_accelerate.py \
     --model_name_or_path "bert-large-uncased" \
     --batch_size 24 \
     --learning_rate 3e-5 \
