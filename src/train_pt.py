f8162d76dba45f9707c19639689fefa25a37c5ff
Gladiator07 <atharvaaingle@gmail.com>
2023-01-23 00:45:34 +0530

    add configuration print

--- src/train_pt.py
+++ src/train_pt.py
@@ -148,7 +148,10 @@ def parse_args():
 def main():
 
     args = parse_args()
+    print(f"===== Configuration =====")
+    print(vars(args))
     set_seed(args.seed)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if args.wandb_enable:
         wandb.init(
