c1255bdd34b691eb017026a12bacd4455d27c80a
Gladiator07 <atharvaaingle@gmail.com>
2023-01-22 17:27:15 +0530

    add 1.13 experiments

---
+++ accelerate_1_0.sh
@@ -0,0 +1,45 @@
+# 1.13 + fp32
+accelerate launch src/train.py \
+    --model_name_or_path "bert-large-uncased" \
+    --batch_size 16 \
+    --learning_rate 2e-5 \
+    --num_epochs 3 \
+    --seed 42 \
+    --mixed_precision "no" \
+    --wandb_enable \
+    --run_name "1.13_fp32"
+
+# 1.13 + fp16
+accelerate launch src/train.py \
+    --model_name_or_path "bert-large-uncased" \
+    --batch_size 24 \
+    --learning_rate 3e-5 \
+    --num_epochs 3 \
+    --seed 42 \
+    --mixed_precision "fp16" \
+    --wandb_enable \
+    --run_name "1.13_fp16"
+
+# 1.13 + fp16 + gradient_checkpointing
+accelerate launch src/train.py \
+    --model_name_or_path "bert-large-uncased" \
+    --batch_size 48 \
+    --learning_rate 3e-5 \
+    --num_epochs 3 \
+    --seed 42 \
+    --gradient_checkpointing \
+    --mixed_precision "fp16" \
+    --wandb_enable \
+    --run_name "1.13_gradckpt"
+
+# 1.13 + fp16 + dynamic_padding
+accelerate launch src/train.py \
+    --model_name_or_path "bert-large-uncased" \
+    --batch_size 24 \
+    --learning_rate 3e-5 \
+    --num_epochs 3 \
+    --seed 42 \
+    --mixed_precision "fp16" \
+    --wandb_enable \
+    --dynamic_padding \
+    --run_name "1.13_dynamic_padding"
