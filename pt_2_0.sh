960419aac3a64e888d9427699f14c0ff103cb663
Gladiator07 <atharvaaingle@gmail.com>
2023-01-23 10:03:36 +0530

    run scripts

--- pt_2_0.sh
+++ pt_2_0.sh
@@ -1,7 +1,34 @@
-# 1.13 + fp32
+# 2.0 + fp32
 python src/train_pt.py \
     --model_name_or_path "bert-large-uncased" \
     --batch_size 16 \
     --learning_rate 2e-5 \
+    --torch_compile \
+    --torch_compile_mode "default" \
+    --torch_compile_backend "inductor" \
     --wandb_enable \
-    --run_name "1.13_fp32"
+    --run_name "2.0_fp32"
+
+# 2.0 + fp16
+python src/train_pt.py \
+    --model_name_or_path "bert-large-uncased" \
+    --batch_size 24 \
+    --learning_rate 3e-5 \
+    --fp16 \
+    --torch_compile \
+    --torch_compile_mode "default" \
+    --torch_compile_backend "inductor" \
+    --wandb_enable \
+    --run_name "2.0_fp16"
+
+# 2.0 + fp16 + reduce-overhead
+python src/train_pt.py \
+    --model_name_or_path "bert-large-uncased" \
+    --batch_size 24 \
+    --learning_rate 3e-5 \
+    --fp16 \
+    --torch_compile \
+    --torch_compile_mode "reduce-overhead" \
+    --torch_compile_backend "inductor" \
+    --wandb_enable \
+    --run_name "2.0_fp16_reduce-overhead"
